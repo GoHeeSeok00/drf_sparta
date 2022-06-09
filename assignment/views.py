ca5f721a907fc9d776998b278872ab9d150bae3a
Github <weeds1590@gmail.com>
2022-06-09 14:59:29 +0900

    Fix: 1일차 과제(views.py) drf형식으로 수정

--- assignment/views.py
+++ assignment/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views import View
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
 
 # Create your views here.
 
 class Assignment(APIView):
-    # permission_classes = permissions.AllowAny
+    permission_classes = [permissions.AllowAny]
     def get(self, request):
-        return HttpResponse({"message": "Success!!!"})
+        data = {
+            "message": "Success!!!"
+        }
+        return Response(data)
