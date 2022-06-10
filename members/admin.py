ccc5d59503fcce4dd4316750aaada31c760ac9be
Github <weeds1590@gmail.com>
2022-06-10 17:47:58 +0900

    [2일차 과제] add - members/models.py 모델 추가 - members/admin.py 어드민 세팅 - members/views.py 기본 세팅, custom permission 만들기

--- members/admin.py
+++ members/admin.py
@@ -1,11 +1,18 @@
-from atexit import register
 from django.contrib import admin
+from django.contrib.auth.models import Group
 
 from members.models import User as UserModel
 from members.models import UserProfile as UserProfileModel
 from members.models import Hobby as HobbyModel
+from members.models import DevLanguage as DevLanguageModel
+from members.models import UserProfileDevLanguage as UserProfileDevLanguage
+
+# Unregister(Group)
+admin.site.unregister(Group)
 
 # Register your models here.
 admin.site.register(UserModel)
 admin.site.register(UserProfileModel)
 admin.site.register(HobbyModel)
+admin.site.register(DevLanguageModel)
+admin.site.register(UserProfileDevLanguage)
--- members/models.py
+++ members/models.py
@@ -11,6 +11,9 @@ class User(models.Model):
 
     level = models.IntegerField("사용자 권한", default=1) # 1: 뉴비 ··· 9: 관리자
 
+    def __str__(self):
+        return f"{self.username} // {self.fullname}"
+
 class UserProfile(models.Model):
     user = models.OneToOneField(to=User, verbose_name="사용자", on_delete=models.CASCADE, primary_key=True)
     hobby = models.ManyToManyField(to="Hobby", verbose_name="취미")
@@ -18,5 +21,24 @@ class UserProfile(models.Model):
     age = models.IntegerField("나이")
     birthday = models.DateField("생일")
 
+    def __str__(self):
+        return f"{self.user.fullname}님의 프로필입니다."
+
 class Hobby(models.Model):
     hobby = models.CharField("취미", max_length=20)
+
+    def __str__(self):
+        return self.hobby
+
+class DevLanguage(models.Model):
+    dev_language = models.CharField("개발 언어", max_length=20)
+
+    def __str__(self):
+        return self.dev_language
+
+class UserProfileDevLanguage(models.Model):
+    userprofile = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    dev_language = models.ForeignKey(DevLanguage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.userprofile.user.fullname} // {self.dev_language}"
