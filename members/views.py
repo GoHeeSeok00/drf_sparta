fc0ab6242d80ce683d077c36437962eafb01701d
Github <weeds1590@gmail.com>
2022-06-11 02:15:28 +0900

    [연습] 역참조, annotate, F, values_list

--- members/views.py
+++ members/views.py
@@ -2,8 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
 
+from django.db.models import F
+
 from members.models import User as UserModel
+from members.models import Hobby as HobbyModel
 from members.models import UserProfile as UserProfileModel
+from members.models import UserProfileDevLanguage as UserProfileDevLanguageModel
 
 # Custom permissions
 class UserLevelPermission(permissions.BasePermission):
@@ -28,12 +32,32 @@ class UserApiView(APIView):
     # permission_classes = [UserLevelPermission]
 
     def get(self, request):
-        users = UserModel.objects.all()
-        print(users)
-        username_list = []  
-        for user in users:
-            username_list.append(user.fullname)                                                                                                                                                                                                                                                                                                                                           
-        return Response(username_list)
+        # users = UserModel.objects.all()
+        users = UserModel.objects.all().values_list("fullname", flat=True) # values_list 실험 코드
+        users = list(users)
+
+        # ★★ 역참조 Test ★★
+        user = UserModel.objects.get(id=1) # result: 오브젝트
+        # print(dir(user))
+        dev_languages = user.userprofile.userprofiledevlanguage_set.all() # result: 쿼리셋 // 정참조 + 역참조
+        # print(dev_languages)
+        for language in dev_languages: # language = object
+            # print(dir(language.userprofile))
+            language_member = language.dev_language.userprofiledevlanguage_set.all().exclude(userprofile=user.userprofile).annotate(userprofile__user__fullname=F('userprofile__user__fullname')).values_list("userprofile__user__fullname", flat=True)
+            language_member = list(language_member)
+            print(f"개발언어: {language.dev_language} / 멤버 : {language_member}")
+
+
+        hobbys = user.userprofile.hobby.all() # result: 쿼리셋 // 정참조
+        # print(hobbys)
+        # print(type(hobbys))
+        for hobby in hobbys:
+            hobby_members = hobby.userprofile_set.exclude(user=user).annotate(fullname=F("user__fullname")).values_list("fullname", flat=True) # 역참조
+            hobby_members = list(hobby_members)
+            print(f"취미: {hobby} / 멤버: {hobby_members}")
+
+        
+        return Response(users)
 
     def post(self, request):
         return Response()
