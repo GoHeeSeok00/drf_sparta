d214237d9472d489b8e5d1cee3606fdbfed09455
Github <weeds1590@gmail.com>
2022-06-12 15:42:53 +0900

    [4일차 과제] Add: blog 모델 어드민 등록 fix: assignment에 작성된 블로그 모델 blog app으로 이동

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from blog.models import Category as CategoryModel
+from blog.models import Article as ArticleModel
+
 # Register your models here.
+admin.site.register(CategoryModel)
+admin.site.register(ArticleModel)
